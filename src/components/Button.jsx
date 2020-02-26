4475a519aa32fb2bf8795679d05bf9dcb7b9803d
nathan-vm <nathan.v.marcelino@gmail.com>
2020-02-26 15:32:42 -0300

    falta imple,mentar logica

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,4 +2,16 @@ import React from 'react'
 import './Button.css'
 
 export default props =>
-<button className="button">{props.label}</button>
+    <button 
+
+        onClick={e => props.click && props.click(props.label)}
+        
+        className={`
+            button
+            ${props.operation ? 'operation' : ''}
+            ${props.double ? 'double' : ''}
+            ${props.triple ? 'triple' : ''}
+        `}>
+
+        {props.label}
+    </button>
